1ff02c8cf12a08c6a8767c9e3621f6c653496c79
isolin0 <46123824+isolin0@users.noreply.github.com>
2019-01-26 16:50:45 -0300

    main menu
    sumo el main manu sin graficos

---
+++ 3andAhB_00/Assets/loadLevel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadLevel : MonoBehaviour
+{
+	public void Load( int sceneNumber)
+	{
+		SceneManager.LoadScene(sceneNumber,LoadSceneMode.Single);
+	}
+}
