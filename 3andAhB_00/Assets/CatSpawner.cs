8f58ea8f2b43aebc2a8c9b8b5632dd640486ef25
isolin0 <mateoisolino@gmail.com>
2019-01-26 05:40:21 -0300

    update 01

---
+++ 3andAhB_00/Assets/CatSpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatSpawner : MonoBehaviour
+{
+	public Transform[] startPosition;
+	public GameObject catPrefab;
+
+	private GameObject catCopy;
+	private float timeBtwMove;
+	public float startTimeBtwMove = 0.25f;
+	public bool stopCatMove;
+	void Start()
+	{
+		int randStartPosition = Random.Range(0, startPosition.Length);
+		transform.position = startPosition[randStartPosition].position;
+		catCopy = GameObject.Instantiate(catPrefab, transform.position, Quaternion.identity) as GameObject;
+
+		
+	}
+
+	void Update()
+	{
+		if (timeBtwMove <= 0 && stopCatMove == false)
+		{
+			Move();
+			timeBtwMove = startTimeBtwMove;
+		}
+		else
+		{
+			timeBtwMove -= Time.deltaTime;
+		}
+	}
+
+	private void Move()
+	{
+		Debug.Log("entro");
+		int randPosition = Random.Range(0, startPosition.Length);
+		catCopy.transform.position = startPosition[randPosition].position;
+
+	}
+	
+
+
+}
---
+++ 3andAhB_00/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+	public float speed;
+	public float jumpForce;
+	private float moveInput;
+
+	public Rigidbody2D rb;
+
+	private bool facingRight = true;
+
+	private bool isGrounded;
+	public Transform groundCheck;
+	public float checkRadius;
+	public LayerMask whatIsGround;
+
+	private int extraJump;
+	public int extraJumpValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+		extraJump = extraJumpValue;
+    }
+	private void Update()
+	{
+		if (isGrounded == true)
+		{
+			extraJump = extraJumpValue;
+		}
+		if(Input.GetKeyDown(KeyCode.UpArrow)&& extraJump > 0)
+		{
+			rb.velocity = Vector2.up * jumpForce;
+			extraJump--;
+		}else if (Input.GetKeyDown(KeyCode.UpArrow) && extraJump == 0 && isGrounded==true)
+		{
+			rb.velocity = Vector2.up * jumpForce;
+		}
+	}
+
+
+	// Update is called once per frame
+	void FixedUpdate()
+    {
+
+		isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+		moveInput = Input.GetAxis("Horizontal");
+		rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+		if(facingRight== false && moveInput < 0)
+		{
+			Flip();
+		}else if (facingRight== true && moveInput > 0)
+		{
+			Flip();
+		}
+    }
+
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 Scaler = transform.localScale;
+		Scaler.x *= -1;
+		transform.localScale = Scaler;
+	
+	}
+}
